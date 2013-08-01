3d177fd96918ab06aae8f85425ad46b1a5faa3d9
Kenneth Kousen <ken.kousen@kousenit.com>
2013-08-01 17:41:53 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('demoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.stuff = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+	  'Angular UI'
+    ];
+  });
